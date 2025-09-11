41bc5df02241ad0351b2e161a70ce4919c40a52e
Zac Pyle <zpyle8687@sdsu.edu>
2025-09-11 12:31:06 -0700

    Topic: Adding Shell Configurations
    Description:
       * Added .bashrc, .vimrc, and fish configuration files

---
+++ config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Add to $PATH
+set -gx PATH $HOME/.juliaup/bin $PATH
+
+# Created by `pipx` on 2025-03-26 16:32:07
+set PATH $PATH /home/zpyle/.local/bin
